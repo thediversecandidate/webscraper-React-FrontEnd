554dee0785dd91e0c7994c9e6c7213c80b174bb0
Gabriele Ferreri <megasoft78@yahoo.it>
2021-04-20 11:21:58 +0200

    Timeline progress

--- src/Components/TimelineComponent/TimelineComponent.tsx
+++ src/Components/TimelineComponent/TimelineComponent.tsx
@@ -1,34 +1,44 @@
-import React from 'react';
 import './TimelineComponent.css';
-import { Chrono } from "react-chrono";
-import { TimelineItemModel } from 'react-chrono/dist/models/TimelineItemModel';
+import { Timeline } from 'primereact/timeline';
+import { Button } from 'primereact/button';
+import { useState } from 'react';
+import { Card } from 'primereact/card';
 
 type TimelineComponentProps = {
     articles: ArticleRow[];
 }
 
 function TimelineComponent({ articles }: TimelineComponentProps) {
+    const [selectedArticle, setSelectedArticle] = useState<ArticleRow>(articles[0]);
 
-    let items: TimelineItemModel[] = [];
+    const customizedMarker = (article: ArticleRow) => {
+        return (
+            <div>
+                <span style={{ position: 'absolute', bottom: '70px', left: '-30px' }} className={'p-text-nowrap p-text-left ' + (selectedArticle.url === article.url ? 'p-text-bold' : '')}>{article.published_date}</span>
+                <Button icon="pi pi-check"
+                    className={'p-button-rounded p-button-timeline ' + (selectedArticle.url === article.url ? 'p-button-success' : '')}
+                    onClick={(e) => setSelectedArticle(article)}
+                />
+            </div>
+        );
+    };
 
-    for (let i = 0; i < articles.length; i++) {
-        const article = articles[i];
-
-        items.push({
-            title: article.published_date,
-            cardTitle: article.title,
-            cardSubtitle: article.article_summary,
-            cardDetailedText: article.body,
-        })
-    }
+    const customizedContent = (article: ArticleRow) => {
+        return (
+            <></>
+        );
+    };
 
     return (
-        <div>
+        <div className="p-pl-5 p-pr-5 p-pt-5">
             {
                 articles.length > 0 &&
-                <Chrono items={items} slideShow slideItemDuration={3000}>
-                </Chrono>
+                <Timeline value={articles} layout="horizontal" align="top"
+                    content={customizedContent} marker={customizedMarker} />
             }
+            <Card title={selectedArticle.title} subTitle={selectedArticle.article_summary} className="p-pt-3">
+                {selectedArticle.body}
+            </Card>
         </div>
     );
 }
